80b09a512d4f24d19841eb0c2510632820f704ce
SunScope Dev <dev@sunscope.app>
2026-03-16 05:36:43 -0500

    feat(sky-detection): package exports

---
+++ packages/sky-detection/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @module @sunscope/sky-detection
+ * @description Sky detection and obstruction mapping for SunScope
+ */
+
+// Types
+export * from './types';
+
+// Sky mask (R-SKY-001)
+export * from './sky-mask';
+
+// Hemisphere stitcher (R-SKY-002)
+export * from './hemisphere-stitcher';
+
+// Arc integrator (R-SKY-003)
+// export * from './arc-integrator';
+
+// Sky classifier (R-SKY-004)
+// export * from './sky-classifier';
